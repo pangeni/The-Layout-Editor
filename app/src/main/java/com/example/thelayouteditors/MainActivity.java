13ba05889505528e3748d9afcb9d84b4c1450749
pangeni <pangenisanjay@gmail.com>
2022-03-18 19:25:51 +0545

    task 2 completed

--- app/src/main/java/com/example/thelayouteditors/MainActivity.java
+++ app/src/main/java/com/example/thelayouteditors/MainActivity.java
@@ -4,43 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int mCount = 0;
-    private TextView mShowCount;
+    private Button button1, button2 ;
+    private TextView text;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
-    }
 
-    /*
-     * Shows a Toast when the TOAST button is clicked.
-     *
-     * @param view The view that triggered this onClick handler.
-     *             Since a toast always shows on the top,
-     *             the passed in view is not used.
-     */
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
-        toast.show();
-    }
 
-    /*
-     * Increments the number in the TextView when the COUNT button is clicked.
-     *
-     * @param view The view that triggered this onClick handler.
-     *             Since the count always appears in the TextView,
-     *             the passed in view is not used.
-     */
-    public void countUp(View view) {
-        mCount++;
-        if (mShowCount != null)
-            mShowCount.setText(Integer.toString(mCount));
+        button1 = findViewById(R.id.Count);
+        button2 = findViewById(R.id.toast);
+
+        text = findViewById(R.id.score);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++ ;
+                text.setText(""+ count);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_LONG).show();
+                count = 0 ;
+                text.setText(""+ count);
+            }
+        });
+
     }
 }
