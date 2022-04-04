2854d0ab12097ae7eec299c69c4073f7e2e44547
pangeni <pangenisanjay@gmail.com>
2022-04-04 13:19:08 +0545

    Hw done

--- app/src/main/java/com/example/thelayouteditors/MainActivity.java
+++ app/src/main/java/com/example/thelayouteditors/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.thelayouteditors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button1, button2 ;
+    private Button button1, button2, button3 ;
     private TextView text;
     private int count;
 
@@ -22,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         button1 = findViewById(R.id.button_count);
         button2 = findViewById(R.id.button_toast);
+        button3 = findViewById(R.id.zero);
 
         text = findViewById(R.id.show_count);
 
@@ -36,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_LONG).show();
+                text.setText(""+ count);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 count = 0 ;
                 text.setText(""+ count);
             }
