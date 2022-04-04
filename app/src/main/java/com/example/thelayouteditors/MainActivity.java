c93c16544f229133c79ba0a1cfe102d7a52ef5bb
pangeni <pangenisanjay@gmail.com>
2022-04-04 21:20:47 +0545

    task completed

--- app/src/main/java/com/example/thelayouteditors/MainActivity.java
+++ app/src/main/java/com/example/thelayouteditors/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.thelayouteditors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private TextView text;
     private int count;
-
+    private Button btn1, btn2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         text = findViewById(R.id.show_count);
+        btn1 = findViewById(R.id.button_count);
+        btn2 = findViewById(R.id.zero);
+
 
     }
 
@@ -28,11 +32,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Count(View view) {
+        btn1.setBackgroundResource(R.color.green);
+        btn2.setBackgroundResource(R.color.red);
         count++ ;
         text.setText(""+ count);
+
     }
 
     public void Zero(View view) {
+        btn1.setBackgroundResource(R.color.black);
+        btn2.setBackgroundResource(R.color.black);
         count = 0 ;
         text.setText(""+ count);
     }
