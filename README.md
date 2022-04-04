3f9ac03a4ff13579aade4978ee21b360eb0dcd9d
pangeni <pangenisanjay@gmail.com>
2022-04-04 16:39:37 +0545

    HW layout

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello Constraint Home Work
+- Aligned two button in the left side of text view.
+- Added new button zero in the middle of those two buttons.
+- Add other two layout for activity(land) and for activity tablet.
+- Zero button helps to make value into zero.
+- The click handler for the count button changes its own background color.
+- Also an zero button changes its background color to set the value into zero.
--- app/src/main/java/com/example/thelayouteditors/MainActivity.java
+++ app/src/main/java/com/example/thelayouteditors/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button1, button2, button3 ;
     private TextView text;
     private int count;
 
@@ -19,33 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = findViewById(R.id.button_count);
-        button2 = findViewById(R.id.button_toast);
-        button3 = findViewById(R.id.zero);
-
         text = findViewById(R.id.show_count);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                count++ ;
-                text.setText(""+ count);
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_LONG).show();
-                text.setText(""+ count);
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                count = 0 ;
-                text.setText(""+ count);
-            }
-        });
+    }
+
+    public void Toast(View view) {
+        Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_LONG).show();
+        text.setText(""+ count);
+    }
+
+    public void Count(View view) {
+        count++ ;
+        text.setText(""+ count);
+    }
+
+    public void Zero(View view) {
+        count = 0 ;
+        text.setText(""+ count);
     }
 }
 
