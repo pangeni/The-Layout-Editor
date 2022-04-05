dad8dac95172713d02f8824459adda09de84d4e0
pangeni <pangenisanjay@gmail.com>
2022-04-05 19:19:30 +0545

    merge hw

---
+++ README.md
@@ -0,0 +1,21 @@
+# Layout Editor Home Work
+- Aligned two button in the left side of text view.
+- Added new button zero in the middle of those two buttons.
+- Adding other two layout for activity(land) and for activity tablet.
+- Adding Zero button helps to make reverse into zero.
+- The click handler for the count button changes its own background color.
+- Button zero button change its background color to set the value into zero.
+## Layout editor in GIF
+![alt text](g.gif)
+
+## Horizontal count
+![alt text](L1.png)
+
+## Horizontal back to zero
+![alt text](l3.png)
+
+## Vertical layout
+![alt text](l4.png)
+
+## Tablet layout
+![alt text](t1.png)
--- app/src/main/java/com/example/thelayouteditors/MainActivity.java
+++ app/src/main/java/com/example/thelayouteditors/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.thelayouteditors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,37 +10,40 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button1, button2 ;
     private TextView text;
     private int count;
+    private Button btn1, btn2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        button1 = findViewById(R.id.button_count);
-        button2 = findViewById(R.id.button_toast);
-
         text = findViewById(R.id.show_count);
+        btn1 = findViewById(R.id.button_count);
+        btn2 = findViewById(R.id.zero);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                count++ ;
-                text.setText(""+ count);
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_LONG).show();
-                count = 0 ;
-                text.setText(""+ count);
-            }
-        });
 
+    }
+
+    public void Toast(View view) {
+        Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_LONG).show();
+        text.setText(""+ count);
+    }
+
+    public void Count(View view) {
+        btn1.setBackgroundResource(R.color.green);
+        btn2.setBackgroundResource(R.color.red);
+        count++ ;
+        text.setText(""+ count);
+
+    }
+
+    public void Zero(View view) {
+        btn1.setBackgroundResource(R.color.teal_200);
+        btn2.setBackgroundResource(R.color.grey);
+        count = 0 ;
+        text.setText(""+ count);
     }
 }
 
